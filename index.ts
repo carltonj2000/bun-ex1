0c104a71f550b199fd625915fa0508967bc9dfd1
Carlton Joseph <carlton.joseph@gmail.com>
2025-01-27 11:54:15 -0800

    add cli options

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
-import { Hono } from "hono";
+import { parseArgs } from "util";
 import { getFreePort } from "./getPort";
+import app from "./app";
 
-const app = new Hono();
-app.get("/", (c) => c.text("Hono!"));
+const { values: args } = parseArgs({
+  args: Bun.argv,
+  options: {
+    browse: {
+      type: "boolean",
+    },
+    freeport: {
+      type: "boolean",
+    },
+  },
+  allowPositionals: true,
+});
 
-const port = await new Promise((resolve, reject) => {
+const freePort = (await new Promise((resolve, reject) => {
   getFreePort(function (err: any, port: any) {
     if (err) reject(err);
     resolve(port);
@@ -12,18 +23,21 @@ const port = await new Promise((resolve, reject) => {
 }).catch((err) => {
   console.error(err);
   process.exit(-1);
+})) as string;
+
+const port = args.freeport ? freePort : process.env.PORT || 3000;
+
+Bun.serve({
+  fetch: app.fetch,
+  port,
 });
 
-export default {
-  port: port,
-  fetch: app.fetch,
-};
-
-const url = `http://localhost:${port}`;
+const url = `http://localhost:${port}/test`;
 const start =
   process.platform == "darwin"
     ? "open"
     : process.platform == "win32"
     ? "start"
     : "xdg-open";
-require("child_process").exec(start + " " + url);
+
+if (args.browse) require("child_process").exec(start + " " + url);
