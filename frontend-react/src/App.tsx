1cbb36f823cdfee4fd20523ef51bb5cb4358a76e
Carlton Joseph <carlton.joseph@gmail.com>
2025-01-28 11:58:06 -0800

    add about route

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   if (error) return <p>Error Receiving Total Spent</p>;
   return (
-    <div className="w-[350px] m-1 p-2 shadow-md shadow-blue-500 rounded-xl mx-auto">
+    <div className="w-[350px] m-2 p-2 shadow-md shadow-blue-500 rounded-xl mx-auto">
       <div className="border-b-2 border-blue-100 flex flex-col items-center gap-2">
         <h1 className="text-xl">Total Spent</h1>
         <p>The total amount you've spent</p>
--- frontend-react/src/routes/__root.tsx
+++ frontend-react/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRoute, Link } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import App from "../App.tsx";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,7 +9,15 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
-      <App />
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr className="h-px bg-blue-700 border-0" />
       <Outlet />
       <TanStackRouterDevtools />
     </React.Fragment>
